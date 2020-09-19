bf5f8c44ae9a2402150c175cc1e293a9c2be9b9b
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2020-09-19 11:39:20 +0530

    debug-3

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ const connect = mongoose.connect(config.mongoURI,
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.use(cors())
+app.use(cors());
+app.options('*',cors());
 
 //to not get any deprecation warning or error
 //support parsing of application/x-www-form-urlencoded post data
